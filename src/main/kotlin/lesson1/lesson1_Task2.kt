ef70b5c876e9cd3dbbca29806edaf8bc03cf74c8
T0xical <vafin.kamil@yandex.ru>
2024-09-13 14:29:57 +0300

    KS-1-2 answer for task

--- src/main/kotlin/lesson1/lesson1_Task2.kt
+++ src/main/kotlin/lesson1/lesson1_Task2.kt
@@ -1,10 +1,16 @@
-package org.example.lesson1
-
-
 fun main(){
-    println("Тестовое решение урок 1 задача 2")
-    println("Чтото доработали, проверяем")
+
+    val countOfOrders: Byte = 75
+    val Thanks: String = "Спасибо за покупку, ждем Вас вновь!"
+
+    println(countOfOrders)
+    println(Thanks)
+
+//    val personalNumber = 2000
+//    println(personalNumber)
+
+    val personalNumber = 1999
 
 
-
+    println(personalNumber)
 }
