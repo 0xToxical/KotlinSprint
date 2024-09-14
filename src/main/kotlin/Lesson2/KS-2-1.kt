b40fd6210b4409addc0883997245630c4399ab8b
T0xical <vafin.kamil@yandex.ru>
2024-09-14 10:24:07 +0300

    KS-2-1 arifmetics

--- src/main/kotlin/Lesson2/KS-2-1.kt
+++ src/main/kotlin/Lesson2/KS-2-1.kt
@@ -1,13 +1,9 @@
-package org.example.Lesson2
-
-fun main(){
-    val a = 3
-    val b = 4
-    val c = 3
-    val d = 5
-    val sum =(a+b+c+d)
-    val AM = sum/4f
-    println (AM)
-
-
+fun main() {
+    val Lena = 3
+    val Anya = 4
+    val Sasha = 3
+    val Dima = 5
+    val sum = (Lena + Anya + Sasha + Dima).toFloat()
+    val ArifMed = (sum / 4)
+    println(ArifMed)
 }
