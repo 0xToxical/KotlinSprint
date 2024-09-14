84495a6fdc45e43da7fd608ebae98a83dea72018
T0xical <vafin.kamil@yandex.ru>
2024-09-14 10:24:07 +0300

    KS-2-1 arithmetic

--- src/main/kotlin/Lesson2/KS-2-1.kt
+++ src/main/kotlin/Lesson2/KS-2-1.kt
@@ -1,13 +1,9 @@
-package org.example.Lesson2
-
-fun main(){
+fun main() {
     val a = 3
     val b = 4
     val c = 3
     val d = 5
-    val sum =(a+b+c+d)
-    val AM = sum/4f
-    println (AM)
-
-
+    val sum = (a + b + c + d)
+    val AM = sum / 4f
+    println(AM)
 }
