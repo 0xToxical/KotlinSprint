77c61b2b8aaaffa2c19c345ae47cc2b1b7d80223
T0xical <vafin.kamil@yandex.ru>
2024-09-15 20:53:29 +0300

    KS-2-1-arithmetic. поменял названия переменных.

--- src/main/kotlin/Lesson2/KS-2-1.kt
+++ src/main/kotlin/Lesson2/KS-2-1.kt
@@ -1,9 +1,9 @@
 fun main() {
-    val Lena = 3
+    val Elena = 3
     val Anya = 4
     val Sasha = 3
     val Dima = 5
-    val sum = (Lena + Anya + Sasha + Dima).toFloat()
+    val sum = (Elena + Anya + Sasha + Dima).toFloat()
     val ArifMed = (sum / 4)
     println(ArifMed)
 }
