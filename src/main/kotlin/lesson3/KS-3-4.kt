e386fdc1f8c33d6acf394cbd58a173a8dcda67ab
T0xical <vafin.kamil@yandex.ru>
2024-09-16 21:38:54 +0300

    KS-3-4-Strings try 1

---
+++ src/main/kotlin/lesson3/KS-3-4.kt
@@ -0,0 +1,14 @@
+fun main() {
+    var from = "E2"
+    var to = "E4"
+    var countOfMove = 1
+
+
+    println("Ход №$countOfMove,$from-$to")
+
+    from = "D2"
+    to = "D3"
+    countOfMove = 3
+
+    println("Ход №$countOfMove,$from-$to")
+}
