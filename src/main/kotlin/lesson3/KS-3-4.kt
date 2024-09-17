34132f16d5e2032204add3e1d9a63d5bc715e8c5
T0xical <vafin.kamil@yandex.ru>
2024-09-17 12:35:21 +0300

    KS-3-4-Strings try 3

--- src/main/kotlin/lesson3/KS-3-4.kt
+++ src/main/kotlin/lesson3/KS-3-4.kt
@@ -1,13 +1,13 @@
 fun main() {
     var from = "E2"
     var to = "E4"
-    var countOfMove = 1
+    var countOfMove = 13
 
-    println("$from-$to, Ход №$countOfMove")
+    println("$from-$to; Ход №$countOfMove")
 
     from = "D2"
     to = "D3"
     countOfMove += 1
 
-    println("$from-$to, Ход №$countOfMove")
+    println("$from-$to; Ход №$countOfMove")
 }
