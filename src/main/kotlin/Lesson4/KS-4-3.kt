4c535ad42b0952b3626d7d4d98453e8d3afe8b32
T0xical <vafin.kamil@yandex.ru>
2024-09-18 02:46:29 +0300

    KS-4-3-comparison try 1

--- src/main/kotlin/Lesson4/KS-4-3.kt
+++ src/main/kotlin/Lesson4/KS-4-3.kt
@@ -1,9 +1,9 @@
-fun main() {
+const val sunnyToday: Boolean = true
+const val awningNow: Boolean = true
+const val airHumidity: Int = 20
+const val partOfYear: String = "winter"
 
-    val sunnyToday = true
-    val awningNow = true
-    val airHumidity = 20
-    val partOfYear = "winter"
+fun main() {
 
     val conditionForBeans = sunnyToday && awningNow && airHumidity == 20 && partOfYear != "winter"
 
