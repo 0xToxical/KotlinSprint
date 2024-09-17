16135721d0a33fd7850e4783ca4034dcaf04a65a
T0xical <vafin.kamil@yandex.ru>
2024-09-17 22:14:04 +0300

    KS-4-1-comparison

---
+++ src/main/kotlin/Lesson4/KS-4-1 comparison.kt
@@ -0,0 +1,12 @@
+const val numberOfTables = 13
+
+fun main() {
+
+    val today = 13
+    val tomorrow = 9
+    val comparisonResult = today < numberOfTables
+    val comparisonResult2 = tomorrow < numberOfTables
+
+    println("Доступность столиков на сегодня: $comparisonResult")
+    println("Доступность столиков на завтра: $comparisonResult2")
+}
