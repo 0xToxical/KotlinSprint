6033bbc71ac2bd7998f39f154a2b5a42af9fb102
T0xical <vafin.kamil@yandex.ru>
2024-09-18 12:52:26 +0300

    KS-4-3-comparison try 2

--- src/main/kotlin/Lesson4/KS-4-3.kt
+++ src/main/kotlin/Lesson4/KS-4-3.kt
@@ -1,13 +1,17 @@
-const val sunnyToday: Boolean = true
-const val awningNow: Boolean = true
-const val airHumidity: Int = 20
-const val partOfYear: String = "winter"
+const val goodSunny: Boolean = true
+const val goodAwning: Boolean = true
+const val goodAirHumidity: Int = 20
+const val badPartOfYear: String = "winter"
 
 fun main() {
+    val sunnyNow: Boolean = true
+    val awningNow: Boolean = true
+    val airHumidityNow: Int = 20
+    val partOfYearNow: String = "winter"
 
-    val conditionForBeans = sunnyToday && awningNow && airHumidity == 20 && partOfYear != "winter"
+    val goodConditionForBeans = (sunnyNow == goodSunny) && (awningNow == goodAwning) && (airHumidityNow == goodAirHumidity) && (partOfYearNow != badPartOfYear)
 
-    println("Благоприятные ли условия сейчас для роста бобовых? $conditionForBeans")
+    println("Благоприятные ли условия сейчас для роста бобовых? $goodConditionForBeans")
 
 
 }
