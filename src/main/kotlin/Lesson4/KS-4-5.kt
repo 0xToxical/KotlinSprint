928d2bce392e00aec6e8c01135866be8cdd18f6e
T0xical <vafin.kamil@yandex.ru>
2024-09-19 20:56:05 +0300

    KS-4-5-comparison try 1

--- src/main/kotlin/Lesson4/KS-4-5.kt
+++ src/main/kotlin/Lesson4/KS-4-5.kt
@@ -1,11 +1,16 @@
 fun main() {
-    var shipIsDamaged: Boolean = false
-    var crewOnShip = 60
+    val shipIsDamaged: Boolean = false
+    val MIN_CREW = 55
+    val MAX_CREW = 70
+    val REQUIREMENT_CREW = 70
+    val MIN_PROVISION = 50
+    val GOOD_WEATHER = true
+
+    val crewOnShip = 60
     var provisions = 51
-    var weatherGood: Boolean = true
 
     val shipCanSailAway =
-        (!shipIsDamaged && crewOnShip in 55..70 && provisions >= 51) || (shipIsDamaged && crewOnShip == 70 && weatherGood && provisions >= 50)
+        (!shipIsDamaged && crewOnShip in MIN_CREW..MAX_CREW && provisions >= MIN_PROVISION) || (shipIsDamaged && crewOnShip == REQUIREMENT_CREW && GOOD_WEATHER && provisions >= provisions)
 
     println("Корабль может отправиться в плавание? $shipCanSailAway")
 
