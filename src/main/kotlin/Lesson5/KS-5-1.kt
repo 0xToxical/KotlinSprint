a4e30fcf53bf0be1a708200d601ed32839613d0b
T0xical <vafin.kamil@yandex.ru>
2024-09-19 11:21:29 +0300

    KS-5-1-If-Else-When

--- src/main/kotlin/Lesson5/KS-5-1.kt
+++ src/main/kotlin/Lesson5/KS-5-1.kt
@@ -1,7 +1,10 @@
 fun main() {
     println("Докажи, что ты не бот, реши задачку:5+4")
+    val a: Int = 5
+    val b: Int = 4
+    val answerTask = a+b
     val example = readln().toInt()
 
-    val resultText = if (example == 9) ("Добро пожаловать") else ("Доступ запрещен")
+    val resultText = if (example == answerTask) ("Добро пожаловать") else ("Доступ запрещен")
     println(resultText)
 }
