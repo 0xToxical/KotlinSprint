79dea8228316e1a98a4c797f8b061685a610c8b2
T0xical <vafin.kamil@yandex.ru>
2024-09-27 21:48:49 +0300

    KS-6-5-cycles try 2

--- src/main/kotlin/Lesson6/KS-6-5.kt
+++ src/main/kotlin/Lesson6/KS-6-5.kt
@@ -1,52 +1,32 @@
-import kotlin.random.Random
-
 fun main() {
-//    println("Для регистрации, введите логин")
-//    val regLogin = readln()
-//    println("Введите пароль")
-//    val regPassword = readln()
-//
-//    println("Вы успешно зарегистрировались!")
-//
-//    var login: String
-//    var password: String
+    var attempts = 3
 
-    var attemps = 3
+    do {
+        val rndmNumber1 = (1..9).random()
+        val rndmNumber2 = (1..9).random()
+        val trueAnswer = rndmNumber1 + rndmNumber2
 
-    val rndmNumber1 = Random.nextInt(1, 10)
-    val rndmNumber2 = Random.nextInt(1, 10)
-    val trueAnswer = rndmNumber1 + rndmNumber2
+        println("Докажите, что Вы не бот, решите задачку: $rndmNumber1 + $rndmNumber2 = ?")
 
-    (println("Докажите, что Вы не бот, решите задачку:$rndmNumber1 + $rndmNumber2 = ?"))
-
-
-    while (attemps > 0) {
-        val answer = readln().toInt()
-        if (answer == trueAnswer) {
-            println("Добро пожаловать!")
-            break
-        } else {
-            attemps--
-            println("Неверно, осталось попыток $attemps")
-            if (attemps > 0) {
-                println("Попробуйте еще раз: $rndmNumber1 + $rndmNumber2 = ?")
+        while (attempts > 0) {
+            val answer = readln().toInt()
+            if (answer == trueAnswer) {
+                println("Добро пожаловать!")
+                return
+            } else {
+                attempts--
+                println("Неверно, осталось попыток $attempts")
+                if (attempts > 0) {
+                    println("Попробуйте еще раз: $rndmNumber1 + $rndmNumber2 = ?")
+                }
             }
         }
-        if (attemps == 0) {
+        if (attempts == 0) {
             println("Доступ запрещен")
-
-//            do {
-//                println("Введите логин")
-//                login = readln()
-//                println("Введите пароль")
-//                password = readln()
-//
-//                if (login != regLogin || password != regPassword) {
-//                    println("Неправильный логин или пароль, попробуйте еще раз")
-//                }
-//            } while (login != regLogin || password != regPassword)
-//
-//            println("Успешная авторизация")
         }
-    }
+    } while (attempts > 0)
 }
+
+
+
+
