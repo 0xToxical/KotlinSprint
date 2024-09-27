dda449de9a86694627f62257be81f40116a0391b
T0xical <vafin.kamil@yandex.ru>
2024-09-27 21:24:24 +0300

    KS-6-4-cycles

--- src/main/kotlin/Lesson6/KS-6-4.kt
+++ src/main/kotlin/Lesson6/KS-6-4.kt
@@ -1,7 +1,5 @@
-import kotlin.random.Random
-
 fun main() {
-    val secretNumber = Random.nextInt(1, 10)
+    val secretNumber = (1..9).random()
     var attempts = 5
 
     println("Угадай число от 1 до 9, у Вас есть $attempts попыток.")
