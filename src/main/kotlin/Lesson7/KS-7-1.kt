5bb8d4d6d1e6621a2375aa683c94bed866417d30
T0xical <vafin.kamil@yandex.ru>
2024-09-28 22:17:32 +0300

    KS-7-1-ranges try 3

--- src/main/kotlin/Lesson7/KS-7-1.kt
+++ src/main/kotlin/Lesson7/KS-7-1.kt
@@ -1,13 +1,14 @@
 fun main() {
-    val numbers = ('0'..'9')
-    val lowerCaseLetters = ('a'..'z')
-
     var password = ""
 
-    repeat(6) {
-        when ((1..2).random()) {
-            1 -> password += numbers.random()
-            2 -> password += lowerCaseLetters.random()
+    val numberRange = 0..9
+    val letterRange = 'a'..'z'
+
+    for (i in 1..6) {
+        if (i % 2 == 0) {
+            password += numberRange.random()
+        } else {
+            password += letterRange.random()
         }
     }
 
