bbbe894da53bf8668d283c83f65624c85a1d907c
T0xical <vafin.kamil@yandex.ru>
2024-09-28 23:41:09 +0300

    KS-8-1-array try 1

--- src/main/kotlin/Lesson8/KS-8-1.kt
+++ src/main/kotlin/Lesson8/KS-8-1.kt
@@ -1,14 +1,12 @@
 fun main() {
-
-    var views = IntArray(7)
+    val views = IntArray(7)
 
     for (i in views.indices) {
         println("Введите количество просмотров за день ${i + 1}:")
         views[i] = readln().toInt()
     }
 
-    var totalViews = 0
-    for (i in views.indices) {
-        totalViews += views[i]
-    }
-println("Общее количество просмотров за неделю: $totalViews")}
+    val totalViews = views.sum()
+
+    println("Общее количество просмотров за неделю: $totalViews")
+}
