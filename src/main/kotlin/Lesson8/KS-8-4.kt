339e2d0a2670ab6303fdc4a5738acc112e19d1db
T0xical <vafin.kamil@yandex.ru>
2024-09-30 07:13:32 +0300

    KS-8-4-array try 1

--- src/main/kotlin/Lesson8/KS-8-4.kt
+++ src/main/kotlin/Lesson8/KS-8-4.kt
@@ -1,25 +1,23 @@
 fun main() {
-
     var ingredients = arrayOf("масло", "рис", "мясо", "лук", "морковь")
     println("Текущий список ингридиентов:")
     ingredients.forEach { println(it) }
-    println("Какой ингридиент хотите изменить?")
-    var oldArray = readln()
 
-    if (ingredients.any { it.equals(oldArray, ignoreCase = true) }) {
+    println("Какой ингридиент хотите изменить?")
+    var oldIngredient = readln()
+
+    val index = ingredients.indexOf(oldIngredient)
+
+    if (index != -1) {
         println("Введите новый ингридиент:")
         val newIngridient = readln()
 
-        val index = ingredients.indexOfFirst { it.equals(oldArray, ignoreCase = true) }
-        if (index != -1) {
-            ingredients[index] = newIngridient
+        ingredients[index] = newIngridient
 
-            println("Готово, вы сохранили следующий список:")
-            ingredients.forEach { println(it) }
-        } else {
-            println("Такого ингридиента в списке нет.")
-        }
+        println("Готово, вы сохранили следующий список:${ingredients.joinToString(", ")}")
+
+    } else {
+        println("Такого ингридиента в списке нет.")
     }
-
-
 }
+
