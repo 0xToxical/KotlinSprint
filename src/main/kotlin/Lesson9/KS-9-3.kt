18273c97299390fa4d5dde4490a864e47f3aa5e4
T0xical <vafin.kamil@yandex.ru>
2024-10-01 20:11:23 +0300

    KS-9-3-collections try 1

--- src/main/kotlin/Lesson9/KS-9-3.kt
+++ src/main/kotlin/Lesson9/KS-9-3.kt
@@ -1,12 +1,17 @@
 fun main() {
+    val EGGS_INDEX = 0
+    val MILK_INDEX = 1
+    val BUTTER_INDEX = 2
 
-    val omletOnePortion = listOf<Int>(2, 50, 15)
+    val omletOnePortion = listOf(2, 50, 15)
+
     println("Сколько порций хотите приготовить?")
     val numberOfPortion = readln().toInt()
 
-    val eggs = omletOnePortion[0] * numberOfPortion
-    val milk = omletOnePortion[1] * numberOfPortion
-    val butter = omletOnePortion[2] * numberOfPortion
+    val ingredientsForPortions = omletOnePortion.map { it * numberOfPortion }
 
-    println("На $numberOfPortion порций, понадобится: яиц - $eggs, молока - $milk, масла - $butter")
+    println("На $numberOfPortion порций, понадобится: " +
+            "яиц - ${ingredientsForPortions[EGGS_INDEX]}, " +
+            "молока - ${ingredientsForPortions[MILK_INDEX]}, " +
+            "масла - ${ingredientsForPortions[BUTTER_INDEX]}")
 }
