24d0a970af8fc90e7398178c7a19214ae6bf68ee
T0xical <vafin.kamil@yandex.ru>
2024-10-01 21:33:25 +0300

    KS-9-3-collections try 1

--- src/main/kotlin/Lesson9/KS-9-4.kt
+++ src/main/kotlin/Lesson9/KS-9-4.kt
@@ -1,9 +1,5 @@
 fun main() {
-
     println("Введите пять ингредиентов, разделенных запятой с пробелом:")
-    val usersIngredient = readln()
-    val ingredientList = usersIngredient.split(", ")
-    val sortedIngredientList = ingredientList.sorted()
-
-    println("Отсортированные ингредиенты $sortedIngredientList")
+    val sortedIngredientList = readln().split(", ").sorted()
+    println("Отсортированные ингредиенты: $sortedIngredientList")
 }
