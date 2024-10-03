5d6630eca857f89e5a3be15991b88c732023f1f5
T0xical <vafin.kamil@yandex.ru>
2024-10-03 22:49:02 +0300

    KS-10-3-functions try 2

--- src/main/kotlin/Lesson10/KS-10-2.kt
+++ src/main/kotlin/Lesson10/KS-10-2.kt
@@ -1,10 +1,12 @@
+const val MIN_LENGTH = 4 // Константа для минимальной длины
+
 fun main() {
-    println("Введите логин, не менее 4-х символов")
+    println("Введите логин, не менее $MIN_LENGTH символов")
     val login = readln()
-    println("Введите пароль, не менее 4-х символов")
+    println("Введите пароль, не менее $MIN_LENGTH символов")
     val password = readln()
 
-    if (login.hasValidLength(4) && password.hasValidLength(4)) {
+    if (login.hasValidLength(MIN_LENGTH) && password.hasValidLength(MIN_LENGTH)) {
         println("Добро пожаловать!")
     } else {
         println("Логин или пароль недостаточно длинные")
