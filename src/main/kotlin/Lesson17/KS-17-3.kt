917ae2b982dc670fbdd7a4169a97247283123f27
T0xical <vafin.kamil@yandex.ru>
2024-10-17 21:46:22 +0300

    KS-17-3-getter\setter

---
+++ src/main/kotlin/Lesson17/KS-17-3.kt
@@ -0,0 +1,20 @@
+class Folder(private val folderName: String,private val numberOfFiles: Int, private val isSecret: Boolean) {
+    val name: String
+        get() = if (isSecret) "Скрытая папка" else folderName
+
+    val files: Int
+        get() = if (isSecret) 0 else numberOfFiles
+}
+
+fun main() {
+    val secretFolder = Folder("Документы", 10, true)
+
+    println("Имя папки: ${secretFolder.name}")
+    println("Количество файлов: ${secretFolder.files}")
+
+
+    val publicFolder = Folder("Фотки", 15, false)
+
+    println("Имя папки: ${publicFolder.name}")
+    println("Количество файлов: ${publicFolder.files}")
+}
