730a519b2f8e7f4d7dc8ba3eddba2e9aa24ba336
T0xical <vafin.kamil@yandex.ru>
2024-10-17 21:35:11 +0300

    KS-17-2-getter\setter

---
+++ src/main/kotlin/Lesson17/KS-17-2.kt
@@ -0,0 +1,26 @@
+class Ship(name: String, var averageSpeed: Double, var homePort: String) {
+    var name: String = name
+        get() = field
+        set(value) {
+            if (value != field) {
+                throw IllegalArgumentException("Ошибка: Имя корабля не может быть изменено!")
+            }
+        }
+}
+
+fun main() {
+    val ship = Ship("Победоносный", 60.0, "Санкт-Петербург")
+
+    // Выводим данные о корабле
+    println("Имя корабля: ${ship.name}")
+    println("Средняя скорость: ${ship.averageSpeed}")
+    println("Порт приписки: ${ship.homePort}")
+
+    try {
+        ship.name = "Гопник Питерский"
+    } catch (e: IllegalArgumentException) {
+        println(e.message)
+    }
+
+    println("Имя корабля после попытки изменения: ${ship.name}")
+}
