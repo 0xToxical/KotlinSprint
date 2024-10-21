738e320d278d682c4c8f8bbba788dfe50110ddf7
T0xical <vafin.kamil@yandex.ru>
2024-10-21 20:42:23 +0300

    KS-18-5-polymorphism try 1

--- src/main/kotlin/Lesson18/18-5.kt
+++ src/main/kotlin/Lesson18/18-5.kt
@@ -1,43 +1,41 @@
-open class Point(val x: Int, val y: Int) {
-    open fun draw() {
-        println("Рисуем точку в координатах: ($x, $y)")
+open class Shape {
+    open fun draw(x: Float, y: Float) {
+        println("Рисуем фигуру в координатах ($x, $y)")
     }
 }
 
-class Square(x: Int, y: Int, val side: Int) : Point(x, y) {
-    override fun draw() {
+class Point : Shape() {
+    override fun draw(x: Float, y: Float) {
+        println("Рисуем точку в координатах ($x, $y)")
+    }
+}
+
+class Square(private val side: Float) : Shape() {
+    override fun draw(x: Float, y: Float) {
         println("Рисуем квадрат с верхним левым углом в ($x, $y) и стороной $side")
     }
 }
 
-class Circle(x: Int, y: Int, val radius: Int) : Point(x, y) {
-    override fun draw() {
+class Circle(private val radius: Float) : Shape() {
+    override fun draw(x: Float, y: Float) {
         println("Рисуем круг с центром в ($x, $y) и радиусом $radius")
     }
 }
 
 class Screen {
-    fun draw(point: Point) {
-        point.draw()
-    }
-
-    fun draw(x: Float, y: Float, point: Point) {
-        println("Рисуем объект с координатами ($x, $y):")
-        point.draw()
+    fun draw(x: Float, y: Float, shape: Shape) {
+        shape.draw(x, y)
     }
 }
 
 fun main() {
     val screen = Screen()
 
-    val point = Point(1, 2)
-    val square = Square(3, 4, 5)
-    val circle = Circle(5, 6, 7)
+    val point = Point()
+    val square = Square(5.0f)
+    val circle = Circle(3.0f)
 
-    screen.draw(point)
-    screen.draw(square)
-    screen.draw(circle)
-
-    screen.draw(1.5f, 2.5f, square)
-    screen.draw(3.5f, 4.5f, circle)
+    screen.draw(1.5f, 2.5f, point)
+    screen.draw(3.5f, 4.5f, square)
+    screen.draw(5.5f, 6.5f, circle)
 }
